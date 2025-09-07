76e38e876147d9c7153e17f1d0060a97f17223d6
MyFGitAccount <coherentlaserbeam@gmail.com>
2025-09-08 01:59:58 +0800

    Update sq.cpp

--- sq.cpp
+++ sq.cpp
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <math.h>
 #include <chrono>
+#include <iomanip>
+
 inline long double square_root(long int num)
 {
  long double answer;
@@ -13,24 +15,23 @@ inline long double square_root(long int num)
 
   while(answer*answer>num)
   {
-     if(answer*answer/1.01/1.01>num){answer/=1.01;}
-     if(answer*answer/1.001/1.001>num){answer/=1.001;}
      if(answer/1.00012*answer/1.00012>num){answer=answer/1.0001;}
-     if(answer*answer*1.0000025*1.0000025>num)answer=answer/1.0000025;
-     answer/=1.00000001;
+     if(answer*answer*1.0000025*1.0000025>num)answer=answer/1.0000001;
   }
   return answer;
 }
 
 int main()
 {// clock_t t=clock();
+  std::ios_base::sync_with_stdio(false);
+
   long int num;
   std::cout<<"enter a number:";
   std::cin>>num;
   auto start = std::chrono::high_resolution_clock::now();
   long double answer=square_root(num);
-  std::cout<<"The square root of "<<num<<" is: "<<answer<<"\n";
-  std::cout<<"THe actual square root of "<<num<<" is: "<<sqrt(num)<<"\n";
+  std::cout<<"The square root of "<<num<<" is: "<<std::fixed<<std::setprecision(12)<<answer<<"\n";
+  std::cout<<"The actual square root of "<<num<<" is: "<<std::fixed<<std::setprecision(12)<<sqrt(num)<<"\n";
   //t=clock()-t;
   auto end = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> elapsed = end - start;
